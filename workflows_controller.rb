867888e3bdf23762d85f29cbb155d44e9a4d1fdb
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-01-28 13:34:01 +0200

    add to_i for coordinates

--- workflows_controller.rb
+++ workflows_controller.rb
@@ -3,7 +3,7 @@ require './lib/biome.rb'
 class WorkflowsController
 
   def get_biome
-    @biome_data = Biome.data Coordinates.new(params[:lat], params[:lng])
+    @biome_data = Biome.data Coordinates.new(params[:lat].to_i, params[:lng].to_i)
     respond_to do |format|
       format.json { render json: @biome_data }
     end
